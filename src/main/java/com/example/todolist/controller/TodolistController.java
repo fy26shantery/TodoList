32b1fea9e9e6c86df670e336dd9425a12534fbef
t.mori <t.mori@shantery.co.jp>
2026-07-17 11:48:10 +0900

    todolist5

--- src/main/java/com/example/todolist/controller/TodolistController.java
+++ src/main/java/com/example/todolist/controller/TodolistController.java
@@ -2,24 +2,129 @@ package com.example.todolist.controller;
 
 import java.util.List;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.todolist.dao.TodoDaoImpl;
 import com.example.todolist.entity.Todo;
+import com.example.todolist.form.TodoData;
+import com.example.todolist.form.TodoQuery;
 import com.example.todolist.repository.TodoRepository;
+import com.example.todolist.service.TodoService;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TodolistController {
 	private final TodoRepository todoRepository;
+	private final TodoService todoService;
+	private final HttpSession session;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	TodoDaoImpl todoDaoImpl;
+
+	@PostConstruct
+	public void init() {
+		todoDaoImpl = new TodoDaoImpl(entityManager);
+	}
 
 	@GetMapping("/todo")
 	public String showTodoList(Model md) {
 		//一覧を検索して表示する
 		List<Todo> todoList = todoRepository.findAll();
+		md.addAttribute("todoList", todoList);
+		md.addAttribute("todoQuery", new TodoQuery());
+
+		return "todoList";
+	}
+
+	//ToDo入力フォーム表示
+	//[処理1]ToDo一覧画面(todoList)で新規追加がクリックされたとき
+	@GetMapping("/todo/create")
+	public String createTodo(Model md) {
+		md.addAttribute("todoData", new TodoData());
+		session.setAttribute("mode", "create");
+		return "todoForm";
+	}
+
+	//ToDo追加処理
+	//[処理2]ToDo入力画面(todoForm)で登録ボタンがクリックされたとき
+	@PostMapping("/todo/create")
+	public String createTodo(@ModelAttribute @Validated TodoData todoData,
+			BindingResult result, Model md) {
+		//エラーチェック
+		boolean isValid = todoService.isValid(todoData, result);
+		if (!result.hasErrors() && isValid) {
+			//エラーなし
+			Todo todo = todoData.toEntity();
+			todoRepository.saveAndFlush(todo);
+			return "redirect:/todo";
+		} else {
+			//エラーあり
+			return "todoForm";
+		}
+	}
+
+	//ToDo処理一覧に戻る
+	//[処理3]Todo入力画面でキャンセル登録がクリックされたとき
+	@PostMapping("/todo/cancel")
+	public String cancel() {
+		return "redirect:/todo";
+	}
+
+	@GetMapping("/todo/{id}")
+	public String todoById(@PathVariable int id, Model md) {
+		Todo todo = todoRepository.findById(id).get();
+		md.addAttribute("todoData", todo);
+		session.setAttribute("mode", "update");
+		return "todoForm";
+	}
+
+	@PostMapping("/todo/update")
+	public String updateTodo(TodoData todoData, BindingResult result, Model md) {
+		//エラーチェック
+		boolean isValid = todoService.isValid(todoData, result);
+		if (!result.hasErrors() && isValid) {
+			//エラーなし
+			Todo todo = todoData.toEntity();
+			todoRepository.saveAndFlush(todo);
+			return "redirect:/todo";
+		} else {
+			//エラーあり
+			return "todoForm";
+		}
+
+	}
+
+	@PostMapping("/todo/delete")
+	public String deleteTodo(TodoData todoData) {
+		todoRepository.deleteById(todoData.getId());
+		return "redirect:/todo";
+	}
+
+	@PostMapping("/todo/query")
+	public String queryTodo(@ModelAttribute TodoQuery todoQuery,
+			BindingResult result, Model md) {
+
+		List<Todo> todoList = null;
+		if (todoService.isValid(todoQuery, result)) {
+			//エラーがなければ検索
+			todoList = todoDaoImpl.findByJPQL(todoQuery);
+		}
+
 		md.addAttribute("todoList", todoList);
 
 		return "todoList";
--- src/main/java/com/example/todolist/entity/Todo.java
+++ src/main/java/com/example/todolist/entity/Todo.java
@@ -7,21 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
-import org.springframework.data.annotation.Id;
-
 import lombok.Data;
 
 @Entity
 @Table(name = "todo")
 @Data
 public class Todo {
-	@Id
+	@jakarta.persistence.Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
 	private String title;
 	private Integer importance;
-	private Integer uregency;
+	private Integer urgency;
 	private Date deadline;
 	private String done;
 
